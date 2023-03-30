bb50162f98bd23da9f16568d6d3f9a7f538ac35e
atrox3d <atrox3d@gmail.com>
2023-03-30 11:45:35 +0200

    35. deque object

--- 07.collections-module/collections-deque.py
+++ 07.collections-module/collections-deque.py
@@ -1,6 +1,23 @@
 import collections
 
-q = collections.deque(
-    [1, 2, 3, 4, 5]
-)
+# create deque
+q = collections.deque(list(range(5, 26, 5)))
+print(q)
+
+# append to the right
+q.append(30)
+print(q)
+
+# append to the left
+q.appendleft(0)
+print(q)
+
+# extracts 1st element on the left
+zero = q.popleft()
+print(zero, q)
+
+# extracts 1st element on the left
+thirty = q.pop()
+print(thirty, q)
+
 
